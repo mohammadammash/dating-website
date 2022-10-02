b20518b1d769a6d3eaaedcf6431e80380971f155
mohammadammash <mohammadammash31@gmail.com>
2022-10-02 16:54:18 +0300

    D:/Git/backend/migrations/create_user_table.php

--- backend/database/migrations/2022_10_02_135058_create_users_table.php
+++ backend/database/migrations/2022_10_02_135058_create_users_table.php
@@ -16,14 +16,17 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('profile_url');
+            $table->string('gender');
+            $table->string('email')->unique();
+            $table->string('interested_in');
+            $table->string('bio');
+            $table->integer('age');
+            $table->string('location');
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
