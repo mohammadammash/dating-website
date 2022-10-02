bbde153f5c51bb56d19f4b63ff82c7d6e9d4a3ac
mohammadammash <mohammadammash31@gmail.com>
2022-10-02 16:55:56 +0300

    D:/Git/backend/migrations/create_blocked_users_table.php

---
+++ backend/database/migrations/2022_10_02_135507_create_blocked_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlockedUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blocked_users', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->integer('blocked_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blocked_users');
+    }
+}
