2717c8c3854b4ae7a1def9e56ad93569c624be62
mohammadammash <mohammadammash31@gmail.com>
2022-10-02 16:59:44 +0300

    /backend/routes/api.php: Intializing needed routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,17 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// LANDING PAGE ROUTES:
+Route::post('/login', [AuthController::class, 'loginUser'])->name('login-user');
+Route::post('/register', [AuthController::class, 'registerUser'])->name('register-user');
+
+// HOME PAGE ROUTES:
+Route::get('/home/{id?}', [UserController::class, 'getUserOrUsers'])->name('get-user-or-users');
+Route::get('/home/id/{state?}', [UserController::class, 'blockOrFollowUser'])->name('block-or-follow-user');
+
+// MESSAGES ROUTES:
+Route::get('/home/id/chat', [MessagesController::class, 'getMessages'])->name('get-messages');
+Route::post('/home/id/chat', [MessagesController::class, 'sendMessage'])->name('send-message');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
