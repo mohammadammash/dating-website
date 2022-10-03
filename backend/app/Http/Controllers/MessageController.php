1ff3492d81ae65a9c21bb8ad448c22d423837fc0
mohammadammash <mohammadammash31@gmail.com>
2022-10-03 19:40:24 +0300

    /backend/MessageController.php/sendMessage(): to send a specific message to a user

--- backend/app/Http/Controllers/MessageController.php
+++ backend/app/Http/Controllers/MessageController.php
@@ -1,15 +1,32 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MessageController extends Controller
 {
-    public function sendMessage(Request $request)
+    public function sendMessage(Request $request, $shown_id)
     {
         $user = JWTAuth::authenticate($request->token);
+        
+        //validate empty message or no receiver id
+        if (!$shown_id or !$request->message) return response()->json([
+            'status' => 'Error',
+            'data'=>'Message Not Sent',
+        ]);
 
-        return $user->id;
+        $data = array(
+            'sender_id' => $user->id, 'receiver_id' => $shown_id, 'text'=>$request->message,
+            'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+            'updated_at' => \Carbon\Carbon::now()->toDateTimeString()
+        );
+        DB::table('messages')->insert($data);
+        return response()->json([
+            'status'=> 'Success',
+            'data'=> 'Message Sent!'
+        ]);
     }
 }
