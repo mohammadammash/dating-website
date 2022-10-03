3035b1c0d8ae48945700eb65b18611c49dac8595
mohammadammash <mohammadammash31@gmail.com>
2022-10-03 18:41:28 +0300

    /backend/routes/api.php: clean the routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,14 +14,16 @@ Route::post('/register', [AuthController::class, 'registerUser'])->name('registe
 Route::group(['middleware' => 'jwt.auth'], function () {
     //log user out remove the token
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout-user');
+
     // show home (get interested gender users - show user own profile)
     Route::get("/home", [UserController::class, "getUsers"])->name("get-users");
     Route::get("/home/profile", [UserController::class, "getUser"])->name("get-user"); //get also all messages received or sent to show in profile page
+
     //specific user - show on click - block or favorite functionality
     Route::get('/home/{shown_id?}', [UserController::class, 'getUser'])->name('get-specific-user');
     Route::post('/home/{shown_id?}', [UserController::class, 'blockOrFollowUser'])->name('block-or-follow-user');
 
-    // send a specific message
+    // send a specific message to a user
     Route::post('/home/{shown_id?}/chat', [MessageController::class, 'sendMessage'])->name('send-message');
 });
 
