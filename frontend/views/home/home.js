5a1b64c0347c9f22f71936cfeb5af827dc1ac83e
mohammadammash <mohammadammash31@gmail.com>
2022-10-04 20:52:34 +0300

    /frontend/views/home/home.js: lear hard coded html content and add function with basic testing to get all users with matching interest gender when page loads

--- frontend/views/home/home.js
+++ frontend/views/home/home.js
@@ -1,4 +1,6 @@
 const logout_button = document.getElementById("logout");
+//home container to add users to:
+const home_container = document.getElementById('home-container');
 
 // check if there is no user return to landing
 const checkCurrentUser = () => {
@@ -11,6 +13,31 @@ const logoutUser = () => {
   checkCurrentUser();
 };
 
-// EVENT LISTENERS
+// FETCH ALL USERS:
+const getAllUsers = ()=>{
+    console.log("let's get users");
+}
+
+// START OF EVENT LISTENERS
 logout_button.addEventListener("click", logoutUser);
 window.addEventListener("load", checkCurrentUser);
+window.addEventListener('load',getAllUsers);
+// END OF EVENT LISTENERS
+
+{/* <div class="card eggshell-bg">
+    <img class="profile-img" src="../../assets/dummy-profile.png">
+    <div>
+        <p>Name: <span class="name">Hadi</span></p>
+        <p>Age: <span class="age">23</span></p>
+    </div>
+    <div>
+        <p>Gender: <span class="gender">Male</span></p>
+        <p>Interested in: <span class="interested-in-gender">Female</span></p>
+    </div>
+    <p class="bio">Hi! My name is Mark, and I am brand new to online dating. While I’m still figuring this all out, here’s something I know for sure—I’m excited to be here!</p>
+    <div class="logos">
+        <img src="../../assets/icons8-favorite-50 (1).png" class="favorite">
+        <img src="../../assets/icons8-chat-room-50.png" class="chat">
+        <img src="../../assets/icons8-block-50.png" class="block">
+    </div>
+</div>  */}
