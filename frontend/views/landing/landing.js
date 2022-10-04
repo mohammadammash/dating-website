d01e1972a0cb28da58d78bd83aec72df6747887d
mohammadammash <mohammadammash31@gmail.com>
2022-10-04 13:04:33 +0300

    /frontend/views/landing/landing.js: Intializing JS File to 'landing.html' with basic selecting of interactive actions buttons

---
+++ frontend/views/landing/landing.js
@@ -0,0 +1,9 @@
+// selecting signup and login modals interactive buttons
+const login_show_button = document.getElementById('login-modal-tn');
+const signup_show_button = document.getElementById("login-modal-btn");
+const login_modal = document.getElementById('login-modal');
+const signup_modal = document.getElementById("signup-modal");
+const close_login_modal = document.getElementById('close-login-modal');
+const close_signup_modal = document.getElementById("close-signup-modal");
+const submit_login_button = document.getElementById('submit-login-button');
+const submit_signup_button = document.getElementById("submit-signup-button");
