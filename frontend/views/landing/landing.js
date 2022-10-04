2de8c5d75fd0908d74a220dcd5d5473b58f346b8
mohammadammash <mohammadammash31@gmail.com>
2022-10-04 13:41:56 +0300

    /frontend/views/landing/landing.js: Selectiong signup form inputs

--- frontend/views/landing/landing.js
+++ frontend/views/landing/landing.js
@@ -5,5 +5,14 @@ const login_modal = document.getElementById('login-modal');
 const signup_modal = document.getElementById("signup-modal");
 const close_login_modal = document.getElementById('close-login-modal');
 const close_signup_modal = document.getElementById("close-signup-modal");
-const submit_login_button = document.getElementById('submit-login-button');
-const submit_signup_button = document.getElementById("submit-signup-button");
+const submit_login = document.getElementById('login-form');
+const submit_signup = document.getElementById("signup-form");
+// signup form inputs:
+const signup_name = document.getElementById('signup-name');
+const signup_email = document.getElementById('signup-email');
+const signup_password = document.getElementById('signup-password');
+const signup_gender = document.getElementById('signup-gender');
+const signup_interested_gender = document.getElementById('signup-interested-gender');
+const signup_age = document.getElementById('signup-age');
+const signup_img_url = document.getElementById('signup-img-url');
+//login form inputs:
