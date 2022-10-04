df2fc4753d24c9384552576b5a3e9f70c4980ef3
mohammadammash <mohammadammash31@gmail.com>
2022-10-04 13:47:58 +0300

    /frontend/views/landing/landing.js: Defining JS event listeners to handle all interaction buttons

--- frontend/views/landing/landing.js
+++ frontend/views/landing/landing.js
@@ -15,6 +15,19 @@ const signup_gender = document.getElementById('signup-gender');
 const signup_interested_gender = document.getElementById('signup-interested-gender');
 const signup_age = document.getElementById('signup-age');
 const signup_img_url = document.getElementById('signup-img-url');
+const signup_img_show = document.getElementById("signup-img-show");
 //login form inputs:
 const login_email = document.getElementById('login-email'); 
 const login_password = document.getElementById("login-password");
+
+
+// START OF EVENT LISTENERS
+//sign up and login submit form:
+submit_login.addEventListener('submit',loginUser);
+submit_signup.addEventListener("submit", signupUser);
+//sign up and login close form:
+close_login_modal.addEventListener('close',closeLoginModal);
+close_signup_modal.addEventListener("close", closeSignupModal);
+// whenever we change the image signup url:
+signup_img_url.addEventListener('change',updateProfileShown);
+// END OF EVENT LISTENERS
