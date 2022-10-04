8c76bbc6651bd32a92a6bf505deabdf0572f4f32
mohammadammash <mohammadammash31@gmail.com>
2022-10-04 20:44:58 +0300

    /frontend/views/home/home.js: logout event listener to clear local storage and return the user to landing page

--- frontend/views/home/home.js
+++ frontend/views/home/home.js
@@ -1,10 +1,15 @@
-const logout_button = document.getElementById('logout');
+const logout_button = document.getElementById("logout");
 
 // check if there is no user return to landing
-const checkCurrentUser = ()=>{
-    const user = document.getElementById('user');
-    if(!user) window.location.href = '../landing/landing.html';
-}
+const checkCurrentUser = () => {
+  const user = localStorage.getItem('user');
+  if(!user) window.location.href = '../landing/landing.html';
+};
+// logout current user:
+const logoutUser = () => {
+  localStorage.clear();
+  checkCurrentUser();
+};
 
 // EVENT LISTENERS
 logout_button.addEventListener("click", logoutUser);
