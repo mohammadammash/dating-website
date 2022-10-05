c2ee4c98de096e4b8c357a807996605d57e786af
mohammadammash <mohammadammash31@gmail.com>
2022-10-05 14:27:27 +0300

    frontend/views/chat/chat.js: adding users html content to chats page in left section

--- frontend/views/chat/chat.js
+++ frontend/views/chat/chat.js
@@ -14,14 +14,23 @@ const logoutUser = () => {
   localStorage.clear();
   checkCurrentUser();
 };
-// add unique user to left chat box with the message:
+// add unique user to left chat box with the message: //show only 5 first charcs of last message
 const addChat = (message) => {
-  console.log(message);
+  const { name, profile_url, text, gender } = message;
+  const htmlChat = `<div class="single-chat eggshell-bg">
+                        <img src="${profile_url}">
+                        <div class="user-info">
+                          <p class="size-20"><span class='name'>${name}</span><span class='gender'>~${gender}</span></p>
+                          <p class="last-msg">${text.slice(0,5)}...</p>
+                        </div>
+                        <button class="btn btn-md">Show</button>
+                      </div>`;
+  all_chats_container.innerHTML += htmlChat;
+  all_chats_container.innerHTML += `<br>`;
 };
 
 //const remove repeated users: //by looping from end of results(latest messages first and getting only last message)
-const showEachUserOnce = (messages) => {
-  const unique_ids = new Set();
+const showEachUserOnce = (messages, unique_ids) => {
   const currentUser = localStorage.getItem("user");
   const id = JSON.parse(currentUser).id;
   for (let i = messages.length - 1; i >= 0; i--) {
@@ -37,6 +46,8 @@ const showEachUserOnce = (messages) => {
       unique_ids.add(sender_id);
     }
   }
+
+  return unique_ids;
 };
 //get All Messages whenever the page loads
 const getAllMessages = async () => {
@@ -44,17 +55,12 @@ const getAllMessages = async () => {
   const token = localStorage.getItem("token");
   const jwt_token = JSON.parse(token);
   const response = await main_object.getAPI(api_url, jwt_token);
-  showEachUserOnce(response.data.messages);
+  const unique_ids = new Set();
+  showEachUserOnce(response.data.messages_sent, unique_ids);
+  showEachUserOnce(response.data.messages_received, unique_ids);
 };
 {
-  /* <div class="single-chat eggshell-bg">
-    <img src="../../assets/dummy-profile.png">
-    <div class="user-info">
-    <p class="size-20"><span class='name'>Hadi </span><span class='gender'>~male</span></p>
-    <p class="last-msg">Okay, meet you arou...</p>
-</div>
-<button class="btn btn-md">Show</button>
-</div> */
+  /*  */
 }
 
 // START OF EVENT LISTENERS
