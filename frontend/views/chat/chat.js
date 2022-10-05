4f91a062a989c0b90562627644273ee08b065193
mohammadammash <mohammadammash31@gmail.com>
2022-10-05 13:35:20 +0300

    frontend/views/chat/chat.js: Removing repeated users, and keep only unique users to show them in chats left section

--- frontend/views/chat/chat.js
+++ frontend/views/chat/chat.js
@@ -1,6 +1,6 @@
 const logout_button = document.getElementById("logout");
 const all_chats_container = document.getElementById("all-chats");
-const show_single_chat_container = document.getElementById('show-chat');
+const show_single_chat_container = document.getElementById("show-chat");
 
 //check if there is a user in localstorage, if not return to landing
 const checkCurrentUser = () => {
@@ -14,29 +14,52 @@ const logoutUser = () => {
   localStorage.clear();
   checkCurrentUser();
 };
+// add unique user to left chat box with the message:
+
+//const remove repeated users: //by looping from end of results(latest messages first and getting only last message)
+const showEachUserOnce = (messages) => {
+  const unique_ids = new Set();
+  const currentUser = localStorage.getItem("user");
+  const id = JSON.parse(currentUser).id;
+  for (let i = messages.length - 1; i >= 0; i--) {
+    const message = messages[i];
+    const sender_id = message.sender_id;
+    const receiver_id = message.receiver_id;
+
+    if (sender_id === id && !unique_ids.has(receiver_id)) {
+      addChat(message);
+      unique_ids.add(receiver_id);
+    } else if (receiver_id === id && !unique_ids.has(sender_id)) {
+      addChat(message);
+      unique_ids.add(sender_id);
+    }
+  }
+};
 //get All Messages whenever the page loads
-const getAllMessages = async ()=>{
+const getAllMessages = async () => {
   const api_url = `${main_object.baseURL}/home/chats`;
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   const jwt_token = JSON.parse(token);
   const response = await main_object.getAPI(api_url, jwt_token);
-  console.log(response.data);
-}
-{/* <div class="single-chat eggshell-bg">
+  showEachUserOnce(response.data.messages);
+};
+{
+  /* <div class="single-chat eggshell-bg">
     <img src="../../assets/dummy-profile.png">
     <div class="user-info">
     <p class="size-20"><span class='name'>Hadi </span><span class='gender'>~male</span></p>
     <p class="last-msg">Okay, meet you arou...</p>
 </div>
 <button class="btn btn-md">Show</button>
-</div> */}
+</div> */
+}
 
 // START OF EVENT LISTENERS
 logout_button.addEventListener("click", logoutUser);
 //on window load
-window.onload = ()=>{
+window.onload = () => {
   //check if there is a user first
   checkCurrentUser();
   //get all messages
   getAllMessages();
-}
+};
