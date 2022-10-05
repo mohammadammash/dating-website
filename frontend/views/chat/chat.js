47ab7ebc0673ae67dfbba6e1dc0d645b44b86e55
mohammadammash <mohammadammash31@gmail.com>
2022-10-05 13:12:59 +0300

    frontend/views/chat/chat.js/getAllMessages(): on page load, get All Messages sent or received by currentUser

--- frontend/views/chat/chat.js
+++ frontend/views/chat/chat.js
@@ -1,17 +1,42 @@
 const logout_button = document.getElementById("logout");
+const all_chats_container = document.getElementById("all-chats");
+const show_single_chat_container = document.getElementById('show-chat');
 
+//check if there is a user in localstorage, if not return to landing
 const checkCurrentUser = () => {
   const user = localStorage.getItem("user");
   if (!user) {
     window.location.href = "../landing/landing.html";
   }
 };
-
+// logout user when logout button is clicked
 const logoutUser = () => {
   localStorage.clear();
   checkCurrentUser();
 };
+//get All Messages whenever the page loads
+const getAllMessages = async ()=>{
+  const api_url = `${main_object.baseURL}/home/chats`;
+  const token = localStorage.getItem('token');
+  const jwt_token = JSON.parse(token);
+  const response = await main_object.getAPI(api_url, jwt_token);
+  console.log(response.data);
+}
+{/* <div class="single-chat eggshell-bg">
+    <img src="../../assets/dummy-profile.png">
+    <div class="user-info">
+    <p class="size-20"><span class='name'>Hadi </span><span class='gender'>~male</span></p>
+    <p class="last-msg">Okay, meet you arou...</p>
+</div>
+<button class="btn btn-md">Show</button>
+</div> */}
 
 // START OF EVENT LISTENERS
 logout_button.addEventListener("click", logoutUser);
-window.addEventListener('load', checkCurrentUser);
+//on window load
+window.onload = ()=>{
+  //check if there is a user first
+  checkCurrentUser();
+  //get all messages
+  getAllMessages();
+}
